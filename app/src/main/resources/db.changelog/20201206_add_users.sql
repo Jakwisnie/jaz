b5719c312a36b1af0ecaee0bc0cafbe69c3683ab
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2021-02-05 15:14:57 +0100

    dasasd

--- app/src/main/resources/db.changelog/20201206_add_users.sql
+++ app/src/main/resources/db.changelog/20201206_add_users.sql
@@ -1,7 +1,2 @@
 --
 
-CREATE TABLE users(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    username VARCHAR NOT NULL,
-    password VARCHAR NOT NULL
-);
--- app/src/main/resources/db.changelog/20201213_add_user_roles.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE userRoles(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    user_role VARCHAR NOT NULL,
-    user_id integer
-);
