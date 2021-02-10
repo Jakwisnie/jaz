76040fcfbdd858e9ffd6d3b3b02b62f4176d3894
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2021-02-10 17:31:18 +0100

    dsadasd

--- app/src/test/java/pl/edu/pjwstk/jaz/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pjwstk.jaz;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
-@Profile({"noSecurity"})
-public class TestConfig {
-}
